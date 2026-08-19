4db72257793b17d69c8708b1c1a33b988d6c66cb
janarakocevic <jana.rakocevic83@gmail.com>
2026-08-19 11:49:21 +0200

    Added property search and filtering

--- backend/controllers/propertiesController.js
+++ backend/controllers/propertiesController.js
@@ -60,5 +60,126 @@ const deleteProperty = async (req, res) => {
     }
 };
 
+const searchProperties = async (req, res) => {
+    try{
+        const q = req.query.q;
+        if(!q){
+            return res.status(400).json({
+                message: "Unesite pojam za pretragu"
+            });
+        }
+        const result = await pool.query(
+            `
+            SELECT
+                p.*,
+                c.cityname,
+                pt.typename
+            FROM properties p
+            JOIN cities c
+                ON p.cityid = c.cityid
+            JOIN propertytypes pt
+                ON p.propertytypeid = pt.propertytypeid
+            WHERE
+                p.title ILIKE $1
+                OR p.description ILIKE $1
+                OR p.address ILIKE $1
+                OR c.cityname ILIKE $1
+                OR pt.typename ILIKE $1
+            `,
+            [`%${q}%`]
+        );
 
-module.exports = { getAllProperties, getPropertyByID, updateProperty, deleteProperty };
+        res.status(200).json(result.rows);
+    }catch(err){
+        console.log(err);
+
+        res.status(500).json({
+            message: "Greska na serveru"
+        });
+    }
+};  
+
+const filterProperties = async (req, res) => {
+    try{
+       const{
+            city,
+            type,
+            minPrice,
+            maxPrice,
+            minArea,
+            rooms
+        } = req.query;
+
+        let query = `
+            SELECT
+                p.*,
+                c.cityname,
+                pt.typename
+            FROM properties p
+            JOIN cities c
+                ON p.cityid = c.cityid
+            JOIN propertytypes pt
+                ON p.propertytypeid = pt.propertytypeid
+            WHERE 1 = 1
+        `;
+
+        const values = [];
+
+        if (city) {
+            values.push(city);
+
+            query += `
+                AND c.cityname ILIKE $${values.length}
+            `;
+        }
+
+        if (type) {
+            values.push(type);
+
+            query += `
+                AND pt.typename ILIKE $${values.length}
+            `;
+        }
+
+        if (minPrice) {
+            values.push(minPrice);
+
+            query += `
+                AND p.price >= $${values.length}
+            `;
+        }
+
+        if (maxPrice) {
+            values.push(maxPrice);
+
+            query += `
+                AND p.price <= $${values.length}
+            `;
+        }
+
+        if (minArea) {
+            values.push(minArea);
+
+            query += `
+                AND p.area >= $${values.length}
+            `;
+        }
+
+        if (rooms) {
+            values.push(rooms);
+
+            query += `
+                AND p.rooms >= $${values.length}
+            `;
+        }
+
+        const result = await pool.query(query, values);
+
+        res.status(200).json(result.rows);
+    }catch(err){
+
+    }
+}
+module.exports = { getAllProperties, getPropertyByID, updateProperty, deleteProperty, searchProperties,
+    filterProperties
+};
--- backend/routes/properties.js
+++ backend/routes/properties.js
@@ -5,10 +5,12 @@ const authMiddleware = require("../middleware/authMiddleware");
 const authorizeRoles = require("../middleware/roleMiddleware");
 
 const {
-    getAllProperties, getPropertyByID, updateProperty, deleteProperty
+    getAllProperties, getPropertyByID, updateProperty, deleteProperty, searchProperties, filterProperties
 } = require("../controllers/propertiesController");
 
 router.get("/", getAllProperties);
+router.get("/search", searchProperties);
+router.get("/filter", filterProperties);
 
 router.get("/:id", getPropertyByID);
 
@@ -21,5 +23,6 @@ router.delete("/:id",
     authMiddleware,
     authorizeRoles("Admin", "Agent"),
     deleteProperty);
+
 
 module.exports = router;
