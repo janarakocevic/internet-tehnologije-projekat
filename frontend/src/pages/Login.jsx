39334ffa9db6950dc2d09aa44d7fed1abceca3c0
janarakocevic <jana.rakocevic83@gmail.com>
2026-08-21 22:21:39 +0200

    Added user registration and login frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,61 @@
+import { useState } from "react";
+import axios from "axios";
 import Navbar from "../components/Navbar";
 
 function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const data = {
+                email, password
+            };
+
+            const response = await axios.post(
+                "http://localhost:3000/auth/login",
+                data
+            );
+            setMessage(response.data.message);
+        }catch(error){
+            console.log(error);
+
+            if(error.response) {
+                setMessage(error.response.data.message);
+            } else{
+                setMessage("Greska prilikom logovanja");
+            }
+        }
+    };
     return(
         <div>
             <Navbar />
             <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email: </label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e)=> setEmail(e.target.value)}
+                        />
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e)=> setPassword(e.target.value)}
+                        />
+                </div>
+                <button type="submit">
+                    Uloguj se
+                </button>
+
+            </form>
+            {message && <p>{message}</p>}
         </div>
     );
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,104 @@
+import { useState } from "react";
+import axios from "axios";
+
 import Navbar from "../components/Navbar";
 
 function Register(){
+
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const data = {
+                firstname,
+                lastname,
+                email,
+                password,
+                phone
+            };
+
+            const response = await axios.post(
+                "http://localhost:3000/auth/register",
+                data
+            );
+
+            setMessage(response.data.message);
+
+            setFirstname("");
+            setLastname("");
+            setEmail("");
+            setPassword("");
+            setPhone("");
+        } catch (error) {
+            console.log(error);
+            if(error.response) {
+                setMessage(error.response.data.message);
+            } else {
+                setMessage("Greska prilikom registracije");
+            }
+        }
+    };
     return(
         <div>
             <Navbar />
-            <h1>Register</h1>
+            <h1>Registracija</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Ime: </label>
+                    <input 
+                        type = "text"
+                        value = {firstname}
+                        onChange={(e) => setFirstname(e.target.value)}
+                        />
+                </div>
+
+                <div>
+                    <label>Prezime: </label>
+                    <input 
+                        type="text"
+                        value = {lastname}
+                        onChange = {(e) => setLastname(e.target.value)}
+                        />
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <input 
+                        type="email"
+                        value = {email}
+                        onChange = {(e) => setEmail(e.target.value)}
+                        />
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input 
+                        type="password"
+                        value = {password}
+                        onChange = {(e) => setPassword(e.target.value)}
+                        />          
+                </div>
+                <div>
+                    <label>Telefon</label>
+                    <input 
+                        type="text"
+                        value = {phone}
+                        onChange = {(e) => setPhone(e.target.value)}
+                        />          
+                </div>
+
+                <button type="submit">
+                    Registruj se
+                </button>
+            </form>
+
+            {message && <p>{message}</p>}
         </div>
     );
 }
