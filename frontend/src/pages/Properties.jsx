0e2b45c86c2d6893d777ca582d491790fb999858
janarakocevic <jana.rakocevic83@gmail.com>
2026-08-22 11:50:24 +0200

    Added property quick search

--- frontend/src/pages/Properties.jsx
+++ frontend/src/pages/Properties.jsx
@@ -6,6 +6,27 @@ import PropertyList from "../components/PropertyList";
 function Properties(){
 
     const [properties, setProperties] = useState([]);
+    const [search, setSearch] = useState("");
+
+    const handleSearch = async () => {
+        try{
+            if(!search.trim()) {
+                const response = await api.get("/properties");
+                setProperties(response.data);
+                return;
+            }
+
+            const response = await api.get("/properties/search", {
+                params: {
+                    q : search
+                }
+            });
+
+            setProperties(response.data);
+        }catch(error){
+            console.log(error);
+        }
+    };
 
     useEffect(() => {
            api.get("/properties")
@@ -19,6 +40,18 @@ function Properties(){
         <div>
             <Navbar />
             <h1>Nekretine</h1>
+
+            <div>
+                <input 
+                    type = "text"
+                    placeholder="Pretrazi nekretnine"
+                    value = {search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+            <button onClick={handleSearch}>
+                Pretrazi
+            </button>
+            </div>
           
             <PropertyList properties={properties} />
                     
