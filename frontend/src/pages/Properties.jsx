c1200eb3a08f674efecd1141f7071e7c2abf0fb5
janarakocevic <jana.rakocevic83@gmail.com>
2026-08-22 12:35:08 +0200

    Added property advanced search

--- frontend/src/pages/Properties.jsx
+++ frontend/src/pages/Properties.jsx
@@ -8,6 +8,13 @@ function Properties(){
     const [properties, setProperties] = useState([]);
     const [search, setSearch] = useState("");
 
+    const [city, setCity] = useState("");
+    const [type, setType] = useState("");
+    const [minPrice, setMinPrice] = useState("");
+    const [maxPrice, setMaxPrice] = useState("");
+    const [minArea, setMinArea] = useState("");
+    const [rooms, setRooms] = useState("");
+
     const handleSearch = async () => {
         try{
             if(!search.trim()) {
@@ -27,6 +34,45 @@ function Properties(){
             console.log(error);
         }
     };
+
+    const handleFilter = async () => {
+        try {
+            const params = {};
+
+            if(city) params.city = city;
+            if(type) params.type = type;
+            if(minPrice) params.minPrice = minPrice;
+            if(maxPrice) params.maxPrice = maxPrice;
+            if(minArea) params.minArea = minArea;
+            if(rooms) params.rooms = rooms;
+
+            const response = await api.get(
+                "/properties/filter",
+                {
+                    params: params
+                }
+            );
+
+            setProperties(response.data);
+        } catch(error) {
+            console.log(error);
+        }
+    };
+    const resetFilters = async () => {
+        setCity("");
+        setType("");
+        setMinPrice("");
+        setMaxPrice("");
+        setMinArea("");
+        setRooms("");
+
+    try {
+        const response = await api.get("/properties");
+        setProperties(response.data);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
     useEffect(() => {
            api.get("/properties")
@@ -52,7 +98,55 @@ function Properties(){
                 Pretrazi
             </button>
             </div>
-          
+
+            <h2>Detaljna pretraga</h2>
+            <div>
+                <input 
+                    type="text"
+                    placeholder="Grad"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                />
+
+                <input 
+                    type="text"
+                    placeholder="Tip nekretnine"
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                />
+                <input 
+                    type="number"
+                    placeholder="Minimalna cijena"
+                    value={minPrice}
+                    onChange={(e) => setMinPrice(e.target.value)}
+                />
+                 <input 
+                    type="number"
+                    placeholder="Maksimalna cijena"
+                    value={maxPrice}
+                    onChange={(e) => setMaxPrice(e.target.value)}
+                />  
+                 <input 
+                    type="number"
+                    placeholder="Minimalna povrsina"
+                    value={minArea}
+                    onChange={(e) => setMinArea(e.target.value)}
+                />        
+                 <input 
+                    type="number"
+                    placeholder="Minimalni broj soba"
+                    value={rooms}
+                    onChange={(e) => setRooms(e.target.value)}
+                />     
+                <button onClick={handleFilter}>
+                    Filtriraj
+                </button> 
+                <button onClick={resetFilters}>
+                    Resetuj filtere
+                </button>                                 
+            </div>
+
+
             <PropertyList properties={properties} />
                     
         </div>
